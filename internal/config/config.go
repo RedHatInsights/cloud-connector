5882aae98ce8c692a23d3de91d693170f00756e0
Derek Horton <dehort@redhat.com>
2021-01-22 15:35:32 -0600

    Make the configurable client id to account id mapper ...configurable

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,36 +11,42 @@ import (
 const (
 	ENV_PREFIX = "CLOUD_CONNECTOR"
 
-	URL_APP_NAME                   = "URL_App_Name"
-	URL_PATH_PREFIX                = "URL_Path_Prefix"
-	URL_BASE_PATH                  = "URL_Base_Path"
-	OPENAPI_SPEC_FILE_PATH         = "OpenAPI_Spec_File_Path"
-	HTTP_SHUTDOWN_TIMEOUT          = "HTTP_Shutdown_Timeout"
-	SERVICE_TO_SERVICE_CREDENTIALS = "Service_To_Service_Credentials"
-	PROFILE                        = "Enable_Profile"
-	BROKERS                        = "Kafka_Brokers"
-	JOBS_TOPIC                     = "Kafka_Jobs_Topic"
-	JOBS_GROUP_ID                  = "Kafka_Jobs_Group_Id"
-	RESPONSES_TOPIC                = "Kafka_Responses_Topic"
-	RESPONSES_BATCH_SIZE           = "Kafka_Responses_Batch_Size"
-	RESPONSES_BATCH_BYTES          = "Kafka_Responses_Batch_Bytes"
-	DEFAULT_BROKER_ADDRESS         = "kafka:29092"
+	URL_APP_NAME                               = "URL_App_Name"
+	URL_PATH_PREFIX                            = "URL_Path_Prefix"
+	URL_BASE_PATH                              = "URL_Base_Path"
+	OPENAPI_SPEC_FILE_PATH                     = "OpenAPI_Spec_File_Path"
+	HTTP_SHUTDOWN_TIMEOUT                      = "HTTP_Shutdown_Timeout"
+	SERVICE_TO_SERVICE_CREDENTIALS             = "Service_To_Service_Credentials"
+	PROFILE                                    = "Enable_Profile"
+	BROKERS                                    = "Kafka_Brokers"
+	JOBS_TOPIC                                 = "Kafka_Jobs_Topic"
+	JOBS_GROUP_ID                              = "Kafka_Jobs_Group_Id"
+	RESPONSES_TOPIC                            = "Kafka_Responses_Topic"
+	RESPONSES_BATCH_SIZE                       = "Kafka_Responses_Batch_Size"
+	RESPONSES_BATCH_BYTES                      = "Kafka_Responses_Batch_Bytes"
+	DEFAULT_BROKER_ADDRESS                     = "kafka:29092"
+	CLIENT_ID_TO_ACCOUNT_ID_IMPL               = "Client_Id_To_Account_Id_Impl"
+	CLIENT_ID_TO_ACCOUNT_ID_CONFIG_FILE        = "Client_Id_To_Account_Id_Config_File"
+	CLIENT_ID_TO_ACCOUNT_ID_DEFAULT_ACCOUNT_ID = "Client_Id_To_Account_Id_Default_Account_Id"
 )
 
 type Config struct {
-	UrlAppName                  string
-	UrlPathPrefix               string
-	UrlBasePath                 string
-	OpenApiSpecFilePath         string
-	HttpShutdownTimeout         time.Duration
-	ServiceToServiceCredentials map[string]interface{}
-	Profile                     bool
-	KafkaBrokers                []string
-	KafkaJobsTopic              string
-	KafkaResponsesTopic         string
-	KafkaResponsesBatchSize     int
-	KafkaResponsesBatchBytes    int
-	KafkaGroupID                string
+	UrlAppName                          string
+	UrlPathPrefix                       string
+	UrlBasePath                         string
+	OpenApiSpecFilePath                 string
+	HttpShutdownTimeout                 time.Duration
+	ServiceToServiceCredentials         map[string]interface{}
+	Profile                             bool
+	KafkaBrokers                        []string
+	KafkaJobsTopic                      string
+	KafkaResponsesTopic                 string
+	KafkaResponsesBatchSize             int
+	KafkaResponsesBatchBytes            int
+	KafkaGroupID                        string
+	ClientIdToAccountIdImpl             string
+	ClientIdToAccountIdConfigFile       string
+	ClientIdToAccountIdDefaultAccountId string
 }
 
 func (c Config) String() string {
@@ -57,6 +63,10 @@ func (c Config) String() string {
 	fmt.Fprintf(&b, "%s: %d\n", RESPONSES_BATCH_SIZE, c.KafkaResponsesBatchSize)
 	fmt.Fprintf(&b, "%s: %d\n", RESPONSES_BATCH_BYTES, c.KafkaResponsesBatchBytes)
 	fmt.Fprintf(&b, "%s: %s\n", JOBS_GROUP_ID, c.KafkaGroupID)
+	fmt.Fprintf(&b, "%s: %s\n", CLIENT_ID_TO_ACCOUNT_ID_IMPL, c.ClientIdToAccountIdImpl)
+	fmt.Fprintf(&b, "%s: %s\n", CLIENT_ID_TO_ACCOUNT_ID_CONFIG_FILE, c.ClientIdToAccountIdConfigFile)
+	fmt.Fprintf(&b, "%s: %s\n", CLIENT_ID_TO_ACCOUNT_ID_DEFAULT_ACCOUNT_ID, c.ClientIdToAccountIdDefaultAccountId)
+
 	return b.String()
 }
 
@@ -75,23 +85,31 @@ func GetConfig() *Config {
 	options.SetDefault(RESPONSES_BATCH_SIZE, 100)
 	options.SetDefault(RESPONSES_BATCH_BYTES, 1048576)
 	options.SetDefault(JOBS_GROUP_ID, "cloud-connector-consumer")
+
+	options.SetDefault(CLIENT_ID_TO_ACCOUNT_ID_IMPL, "config_file_based")
+	options.SetDefault(CLIENT_ID_TO_ACCOUNT_ID_CONFIG_FILE, "client_id_to_account_id_map.json")
+	options.SetDefault(CLIENT_ID_TO_ACCOUNT_ID_DEFAULT_ACCOUNT_ID, "111000")
+
 	options.SetEnvPrefix(ENV_PREFIX)
 	options.AutomaticEnv()
 
 	return &Config{
-		UrlPathPrefix:               options.GetString(URL_PATH_PREFIX),
-		UrlAppName:                  options.GetString(URL_APP_NAME),
-		UrlBasePath:                 buildUrlBasePath(options.GetString(URL_PATH_PREFIX), options.GetString(URL_APP_NAME)),
-		OpenApiSpecFilePath:         options.GetString(OPENAPI_SPEC_FILE_PATH),
-		HttpShutdownTimeout:         options.GetDuration(HTTP_SHUTDOWN_TIMEOUT) * time.Second,
-		ServiceToServiceCredentials: options.GetStringMap(SERVICE_TO_SERVICE_CREDENTIALS),
-		Profile:                     options.GetBool(PROFILE),
-		KafkaBrokers:                options.GetStringSlice(BROKERS),
-		KafkaJobsTopic:              options.GetString(JOBS_TOPIC),
-		KafkaResponsesTopic:         options.GetString(RESPONSES_TOPIC),
-		KafkaResponsesBatchSize:     options.GetInt(RESPONSES_BATCH_SIZE),
-		KafkaResponsesBatchBytes:    options.GetInt(RESPONSES_BATCH_BYTES),
-		KafkaGroupID:                options.GetString(JOBS_GROUP_ID),
+		UrlPathPrefix:                       options.GetString(URL_PATH_PREFIX),
+		UrlAppName:                          options.GetString(URL_APP_NAME),
+		UrlBasePath:                         buildUrlBasePath(options.GetString(URL_PATH_PREFIX), options.GetString(URL_APP_NAME)),
+		OpenApiSpecFilePath:                 options.GetString(OPENAPI_SPEC_FILE_PATH),
+		HttpShutdownTimeout:                 options.GetDuration(HTTP_SHUTDOWN_TIMEOUT) * time.Second,
+		ServiceToServiceCredentials:         options.GetStringMap(SERVICE_TO_SERVICE_CREDENTIALS),
+		Profile:                             options.GetBool(PROFILE),
+		KafkaBrokers:                        options.GetStringSlice(BROKERS),
+		KafkaJobsTopic:                      options.GetString(JOBS_TOPIC),
+		KafkaResponsesTopic:                 options.GetString(RESPONSES_TOPIC),
+		KafkaResponsesBatchSize:             options.GetInt(RESPONSES_BATCH_SIZE),
+		KafkaResponsesBatchBytes:            options.GetInt(RESPONSES_BATCH_BYTES),
+		KafkaGroupID:                        options.GetString(JOBS_GROUP_ID),
+		ClientIdToAccountIdImpl:             options.GetString(CLIENT_ID_TO_ACCOUNT_ID_IMPL),
+		ClientIdToAccountIdConfigFile:       options.GetString(CLIENT_ID_TO_ACCOUNT_ID_CONFIG_FILE),
+		ClientIdToAccountIdDefaultAccountId: options.GetString(CLIENT_ID_TO_ACCOUNT_ID_DEFAULT_ACCOUNT_ID),
 	}
 }
 
--- internal/controller/account_resolver.go
+++ internal/controller/account_resolver.go
@@ -2,13 +2,32 @@ package controller
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"os"
 
+	"github.com/RedHatInsights/cloud-connector/internal/config"
 	"github.com/RedHatInsights/cloud-connector/internal/domain"
+	"github.com/RedHatInsights/cloud-connector/internal/platform/logger"
 )
 
 type AccountIdResolver interface {
 	MapClientIdToAccountId(context.Context, domain.ClientID) (domain.AccountID, error)
+}
+
+func NewAccountIdResolver(accountIdResolverImpl string, cfg *config.Config) (AccountIdResolver, error) {
+	switch accountIdResolverImpl {
+	case "config_file_based":
+		resolver := ConfigurableAccountIdResolver{Config: cfg}
+		err := resolver.init()
+		return &resolver, err
+	case "bop":
+		return &BOPAccountIdResolver{}, nil
+	default:
+		return nil, errors.New("Invalid AccountIdResolver impl requested")
+	}
 }
 
 type BOPAccountIdResolver struct {
@@ -41,16 +60,54 @@ func (bar *BOPAccountIdResolver) MapClientIdToAccountId(ctx context.Context, cli
 }
 
 type ConfigurableAccountIdResolver struct {
+	Config                 *config.Config
+	clientIdToAccountIdMap map[domain.ClientID]domain.AccountID
+	defaultAccountId       domain.AccountID
+}
+
+func (bar *ConfigurableAccountIdResolver) init() error {
+
+	err := bar.loadAccountIdMapFromFile()
+	if err != nil {
+		return err
+	}
+
+	bar.defaultAccountId = domain.AccountID(bar.Config.ClientIdToAccountIdDefaultAccountId)
+
+	return nil
+}
+
+func (bar *ConfigurableAccountIdResolver) loadAccountIdMapFromFile() error {
+
+	logger.Log.Debug("Loading Client Id to Account Id config file: ", bar.Config.ClientIdToAccountIdConfigFile)
+
+	configFile, err := os.Open(bar.Config.ClientIdToAccountIdConfigFile)
+	if err != nil {
+		logger.Log.Error("Could not load account resolver config file: ", err)
+		return err
+	}
+	defer configFile.Close()
+
+	jsonBytes, err := ioutil.ReadAll(configFile)
+	if err != nil {
+		logger.Log.Error("Could not load account resolver config file: ", err)
+		return err
+	}
+
+	err = json.Unmarshal(jsonBytes, &bar.clientIdToAccountIdMap)
+	if err != nil {
+		logger.Log.Error("Could not parse account resolver config file: ", err)
+		return err
+	}
+
+	return nil
 }
 
 func (bar *ConfigurableAccountIdResolver) MapClientIdToAccountId(ctx context.Context, clientID domain.ClientID) (domain.AccountID, error) {
-	// FIXME:  Make this configurable...this should be helpful in testing until we can get BOP / 3scale wired up correctly
-	switch clientID {
-	case "client-0":
-		return domain.AccountID("010101"), nil
-	case "client-1":
-		return domain.AccountID("010102"), nil
-	default:
-		return domain.AccountID("0000001"), nil
+
+	if accountId, ok := bar.clientIdToAccountIdMap[clientID]; ok == true {
+		return accountId, nil
 	}
+
+	return bar.defaultAccountId, nil
 }
