d5eacec2b10ba5fa434c9c8fdb72267ec0ba1cf7
Derek Horton <dehort@redhat.com>
2021-05-27 10:39:51 -0500

    Starting to split up the mqtt consummer and kafka consumer

---
+++ cmd/cloud-connector/kafka_message_consumer.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/RedHatInsights/cloud-connector/internal/config"
+	"github.com/RedHatInsights/cloud-connector/internal/controller"
+	"github.com/RedHatInsights/cloud-connector/internal/controller/api"
+	"github.com/RedHatInsights/cloud-connector/internal/mqtt"
+	"github.com/RedHatInsights/cloud-connector/internal/platform/logger"
+    "github.com/RedHatInsights/cloud-connector/internal/platform/queue"
+	"github.com/RedHatInsights/cloud-connector/internal/platform/utils"
+	"github.com/RedHatInsights/cloud-connector/internal/platform/utils/tls_utils"
+
+	"github.com/gorilla/mux"
+)
+
+func startKafkaMessageConsumer(mgmtAddr string) {
+
+	logger.InitLogger()
+
+	logger.Log.Info("Starting Cloud-Connector Kafka Message consumer")
+
+	cfg := config.GetConfig()
+	logger.Log.Info("Cloud-Connector configuration:\n", cfg)
+
+	tlsConfigFuncs, err := buildBrokerTlsConfigFuncList(cfg)
+	if err != nil {
+		logger.LogFatalError("TLS configuration error for MQTT Broker connection", err)
+	}
+
+	tlsConfig, err := tls_utils.NewTlsConfig(tlsConfigFuncs...)
+	if err != nil {
+		logger.LogFatalError("Unable to configure TLS for MQTT Broker connection", err)
+	}
+
+	sqlConnectionRegistrar, err := controller.NewSqlConnectionRegistrar(cfg)
+	if err != nil {
+		logger.LogFatalError("Failed to create SQL Connection Registrar", err)
+	}
+
+	accountResolver, err := controller.NewAccountIdResolver(cfg.ClientIdToAccountIdImpl, cfg)
+	if err != nil {
+		logger.LogFatalError("Failed to create Account ID Resolver", err)
+	}
+
+	connectedClientRecorder, err := controller.NewConnectedClientRecorder(cfg.ConnectedClientRecorderImpl, cfg)
+	if err != nil {
+		logger.LogFatalError("Failed to create Connected Client Recorder", err)
+	}
+
+	sourcesRecorder, err := controller.NewSourcesRecorder(cfg.SourcesRecorderImpl, cfg)
+	if err != nil {
+		logger.LogFatalError("Failed to create Sources Recorder", err)
+	}
+
+	mqttTopicBuilder := mqtt.NewTopicBuilder(cfg.MqttTopicPrefix)
+	mqttTopicVerifier := mqtt.NewTopicVerifier(cfg.MqttTopicPrefix)
+
+	// FIXME:
+	//start kafka consumer
+	rhcMessageKafkaConsumer := queue.ConsumerConfig{
+		Brokers: []string{"localhost:29092"},
+		Topic:   "platform.cloud-connector.rhc-messages",
+		GroupID: "cloud-connector-rhc-message-consumer",
+	}
+	kafkaReader := queue.StartConsumer(&rhcMessageKafkaConsumer)
+
+	brokerOptions, err := buildMessageHandlerMqttBrokerConfigFuncList(cfg.MqttBrokerAddress, tlsConfig, cfg)
+	if err != nil {
+		logger.LogFatalError("Unable to configure MQTT Broker connection", err)
+	}
+
+	apiMux := mux.NewRouter()
+
+	monitoringServer := api.NewMonitoringServer(apiMux, cfg)
+	monitoringServer.Routes()
+
+	apiSrv := utils.StartHTTPServer(mgmtAddr, "management", apiMux)
+
+	signalChan := make(chan os.Signal, 1)
+
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	sig := <-signalChan
+	logger.Log.Info("Received signal to shutdown: ", sig)
+
+	ctx, cancel := context.WithTimeout(context.Background(), cfg.HttpShutdownTimeout)
+	defer cancel()
+
+	utils.ShutdownHTTPServer(ctx, "management", apiSrv)
+
+	logger.Log.Info("Cloud-Connector shutting down")
+}
