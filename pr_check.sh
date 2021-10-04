1bc33e492a198044810260415390adf7db19457b
Rodrigo Antunes <rantunes@redhat.com>
2021-10-04 10:23:49 -0300

    Update pr_check.sh

--- pr_check.sh
+++ pr_check.sh
@@ -19,6 +19,5 @@ curl -s $CICD_URL/bootstrap.sh -o bootstrap.sh
 source bootstrap.sh  # checks out bonfire and changes to "cicd" dir...
 
 source $CICD_ROOT/build.sh
-source $APP_ROOT/unit_test.sh
 source $CICD_ROOT/deploy_ephemeral_env.sh
 source $CICD_ROOT/cji_smoke_test.sh
