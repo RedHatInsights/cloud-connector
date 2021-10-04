3f026c383469c42ddb62a1f1348a0d7b56a0b7a9
Rodrigo Antunes <rantunes@redhat.com>
2021-10-04 10:09:50 -0300

    Enable smoke tests

--- pr_check.sh
+++ pr_check.sh
@@ -7,9 +7,9 @@ APP_NAME="cloud-connector"  # name of app-sre "application" folder this componen
 COMPONENT_NAME="cloud-connector"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
 IMAGE="quay.io/cloudservices/cloud-connector"
 
-IQE_PLUGINS="iqe-rhc-plugin"
-IQE_MARKER_EXPRESSION="smoke"
-IQE_FILTER_EXPRESSION="cloud-connector"
+IQE_PLUGINS="rhc"
+IQE_MARKER_EXPRESSION=""
+IQE_FILTER_EXPRESSION="cloud_connector"
 IQE_CJI_TIMEOUT="10m"
 
 
@@ -21,4 +21,4 @@ source bootstrap.sh  # checks out bonfire and changes to "cicd" dir...
 source $CICD_ROOT/build.sh
 source $APP_ROOT/unit_test.sh
 source $CICD_ROOT/deploy_ephemeral_env.sh
-#source $CICD_ROOT/cji_smoke_test.sh
+source $CICD_ROOT/cji_smoke_test.sh
