c4a10bc22a846dd43518ac28817c0e2f3af26f78
Rodrigo <contato@rodrigoantunes.com.br>
2021-11-11 11:19:33 -0300

    Update unit_test.sh according to Bonfire requirements (#100)
    * Update unit_test.sh according to Bonfire requirements
    * Remove unit_test.sh mock file
    * Update pr_check.sh component name
    * Update pr_check.sh script
    
    Co-authored-by: Rodrigo Antunes <rantunes@redhat.com>

--- pr_check.sh
+++ pr_check.sh
@@ -4,7 +4,7 @@
 # Options that must be configured by app owner
 # --------------------------------------------
 APP_NAME="cloud-connector"  # name of app-sre "application" folder this component lives in
-COMPONENT_NAME="cloud-connector"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+COMPONENT_NAME="cloud-connector-api"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
 IMAGE="quay.io/cloudservices/cloud-connector"
 
 IQE_PLUGINS="rhc"
@@ -12,12 +12,12 @@ IQE_MARKER_EXPRESSION=""
 IQE_FILTER_EXPRESSION="cloud_connector"
 IQE_CJI_TIMEOUT="10m"
 
-
 # Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
-curl -s $CICD_URL/bootstrap.sh -o bootstrap.sh
-source bootstrap.sh  # checks out bonfire and changes to "cicd" dir...
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
 source $CICD_ROOT/build.sh
 source $CICD_ROOT/deploy_ephemeral_env.sh
+
+COMPONENT_NAME="cloud-connector"
 source $CICD_ROOT/cji_smoke_test.sh
