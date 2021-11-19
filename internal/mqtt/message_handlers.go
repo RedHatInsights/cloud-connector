9b79518b2e27b2cfde11d4472dace762984568c5
Derek Horton <dehort@redhat.com>
2021-11-19 16:53:00 -0600

    Renaming some files and a bit of reorganizing

--- internal/mqtt/message_handlers.go
+++ internal/mqtt/message_handlers.go
@@ -18,53 +18,6 @@ const (
 	MessageIDKafkaHeaderKey = "mqtt_message_id"
 )
 
-type Subscriber struct {
-	Topic      string
-	EntryPoint MQTT.MessageHandler
-	Qos        byte
-}
-
-func CreateBrokerConnection(brokerUrl string, onConnectHandler func(MQTT.Client), brokerConfigFuncs ...MqttClientOptionsFunc) (MQTT.Client, error) {
-
-	connOpts, err := NewBrokerOptions(brokerUrl, brokerConfigFuncs...)
-	if err != nil {
-		logger.Log.WithFields(logrus.Fields{"error": err}).Error("Unable to build MQTT ClientOptions")
-		return nil, err
-	}
-
-	connOpts.SetOnConnectHandler(onConnectHandler)
-
-	mqttClient := MQTT.NewClient(connOpts)
-	if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
-		logger.Log.WithFields(logrus.Fields{"error": token.Error()}).Error("Unable to connect to MQTT broker")
-		return nil, token.Error()
-	}
-
-	logger.Log.Info("Connected to MQTT broker: ", brokerUrl)
-
-	return mqttClient, nil
-}
-
-func RegisterSubscribers(brokerUrl string, subscribers []Subscriber, defaultMessageHandler func(MQTT.Client, MQTT.Message), brokerConfigFuncs ...MqttClientOptionsFunc) (MQTT.Client, error) {
-
-	// Add a default publish message handler as some messages will get delivered before the topic
-	// subscriptions are setup completely
-	// See "Common Problems" here: https://github.com/eclipse/paho.mqtt.golang#common-problems
-	brokerConfigFuncs = append(brokerConfigFuncs, WithDefaultPublishHandler(defaultMessageHandler))
-
-	return CreateBrokerConnection(
-		brokerUrl,
-		func(client MQTT.Client) {
-			for _, subscriber := range subscribers {
-				logger.Log.Infof("Subscribing to MQTT topic: %s - QOS: %d\n", subscriber.Topic, subscriber.Qos)
-				if token := client.Subscribe(subscriber.Topic, subscriber.Qos, subscriber.EntryPoint); token.Wait() && token.Error() != nil {
-					logger.Log.WithFields(logrus.Fields{"error": token.Error()}).Fatalf("Subscribing to MQTT topic (%s) failed", subscriber.Topic)
-				}
-			}
-		},
-		brokerConfigFuncs...)
-}
-
 func ControlMessageHandler(ctx context.Context, kafkaWriter *kafka.Writer, topicVerifier *TopicVerifier) func(MQTT.Client, MQTT.Message) {
 	return func(client MQTT.Client, message MQTT.Message) {
 
