8eb01f78cd31b4fb98d8c184e78cebf23a1d64e7
Rodrigo Antunes <rantunes@redhat.com>
2022-04-01 13:32:37 -0300

    Run RHC contract tests as part of the PR Check job

--- pr_check.sh
+++ pr_check.sh
@@ -6,11 +6,7 @@
 APP_NAME="cloud-connector"  # name of app-sre "application" folder this component lives in
 COMPONENT_NAME="cloud-connector-api"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
 IMAGE="quay.io/cloudservices/cloud-connector"
-
-IQE_PLUGINS="cloud-connector"
-IQE_MARKER_EXPRESSION=""
-IQE_FILTER_EXPRESSION=""
-IQE_CJI_TIMEOUT="10m"
+EXTRA_DEPLOY_ARGS="-p playbook-dispatcher/CLOUD_CONNECTOR_IMPL=impl"
 
 # Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
@@ -20,4 +16,12 @@ source $CICD_ROOT/build.sh
 source $CICD_ROOT/deploy_ephemeral_env.sh
 
 COMPONENT_NAME="cloud-connector"
+
+# Run Cloud Connector isolated tests
+IQE_PLUGINS="cloud-connector"
+source $CICD_ROOT/cji_smoke_test.sh
+
+# Run RHC Contract integration tests
+IQE_PLUGINS="rhc-contract"
+IQE_IMAGE_TAG="rhc-contract"
 source $CICD_ROOT/cji_smoke_test.sh
