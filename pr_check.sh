f474918ad58c8f5c8da77d97cc57f101da73a41c
Rodrigo <contato@rodrigoantunes.com.br>
2022-04-07 11:39:22 -0300

    Run RHC contract tests as part of the PR Check job (#133)
    * Run RHC contract tests as part of the PR Check job
    * Add IQE_CJI_TIMEOUT
    * Change component name
    * Add REF_ENV var
    
    Co-authored-by: Rodrigo Antunes <rantunes@redhat.com>

--- pr_check.sh
+++ pr_check.sh
@@ -6,11 +6,9 @@
 APP_NAME="cloud-connector"  # name of app-sre "application" folder this component lives in
 COMPONENT_NAME="cloud-connector-api"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
 IMAGE="quay.io/cloudservices/cloud-connector"
-
-IQE_PLUGINS="cloud-connector"
-IQE_MARKER_EXPRESSION=""
-IQE_FILTER_EXPRESSION=""
 IQE_CJI_TIMEOUT="10m"
+EXTRA_DEPLOY_ARGS="-p playbook-dispatcher/CLOUD_CONNECTOR_IMPL=impl"
+REF_ENV="insights-stage"
 
 # Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
@@ -19,5 +17,13 @@ curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 source $CICD_ROOT/build.sh
 source $CICD_ROOT/deploy_ephemeral_env.sh
 
+# Run Cloud Connector isolated tests
 COMPONENT_NAME="cloud-connector"
+IQE_PLUGINS="cloud-connector"
+source $CICD_ROOT/cji_smoke_test.sh
+
+# Run RHC Contract integration tests
+COMPONENT_NAME="playbook-dispatcher"
+IQE_PLUGINS="rhc-contract"
+IQE_IMAGE_TAG="rhc-contract"
 source $CICD_ROOT/cji_smoke_test.sh
