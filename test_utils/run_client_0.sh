d0e90f9ab213e6cac13f2bbb472eecbed368ac26
Derek Horton <dehort@redhat.com>
2022-08-31 14:33:01 -0500

    Adding scripts for local testing

---
+++ test_utils/run_client_0.sh
@@ -0,0 +1,13 @@
+
+# Port-forward
+#BROKER="tcp://localhost:9883"
+
+BROKER="ssl://mosquitto-route-connector-ci.5a9f.insights-dev.openshiftapps.com:443"
+BROKER="ssl://localhost:8883"
+
+WORKING_DIR=/home/dehort/dev/go/src/github.com/RedHatInsights/cloud-connector
+WORKING_DIR=./dev/test_client
+
+./test_client -broker $BROKER -connection_count 1 -cert ${WORKING_DIR}/client-0-cert.pem -key ${WORKING_DIR}/client-0-key.pem
+
+
