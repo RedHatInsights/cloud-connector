815c3cd8377ed280dc1fca88a70f95c2b680adad
Cody Mitchell <comitche@redhat.com>
2022-10-04 13:54:16 -0400

    Add canonical facts and tags to mock proxy interface

--- internal/controller/api/message_receiver_test.go
+++ internal/controller/api/message_receiver_test.go
@@ -35,7 +35,7 @@ const (
 type MockClientProxyFactory struct {
 }
 
-func (MockClientProxyFactory) CreateProxy(context.Context, domain.OrgID, domain.AccountID, domain.ClientID, domain.Dispatchers) (controller.ConnectorClient, error) {
+func (MockClientProxyFactory) CreateProxy(context.Context, domain.OrgID, domain.AccountID, domain.ClientID, domain.CanonicalFacts, domain.Dispatchers, domain.Tags) (controller.ConnectorClient, error) {
 	return MockClient{}, nil
 }
 
