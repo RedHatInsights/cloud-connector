db1c688e8ec77bad7a3b4fddcd71a81a61baa6e3
Derek Horton <dehort@redhat.com>
2022-10-17 13:42:14 -0500

    Add timeout value to the mqtt token.Wait() [RHCLOUD-21244]

--- internal/mqtt/utils.go
+++ internal/mqtt/utils.go
@@ -3,6 +3,7 @@ package mqtt
 import (
 	"bytes"
 	"encoding/json"
+	"time"
 
 	"github.com/RedHatInsights/cloud-connector/internal/cloud_connector/protocol"
 	"github.com/RedHatInsights/cloud-connector/internal/domain"
@@ -13,7 +14,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func SendReconnectMessageToClient(mqttClient MQTT.Client, logger *logrus.Entry, topicBuilder *TopicBuilder, qos byte, clientID domain.ClientID, delay int) error {
+func SendReconnectMessageToClient(mqttClient MQTT.Client, logger *logrus.Entry, topicBuilder *TopicBuilder, qos byte, publishTimeout time.Duration, clientID domain.ClientID, delay int) error {
 
 	messageID, message, err := protocol.BuildReconnectMessage(delay)
 
@@ -27,12 +28,12 @@ func SendReconnectMessageToClient(mqttClient MQTT.Client, logger *logrus.Entry, 
 
 	topic := topicBuilder.BuildOutgoingControlTopic(clientID)
 
-	err = sendMessage(mqttClient, logger, clientID, messageID, topic, qos, message)
+	err = sendMessage(mqttClient, logger, clientID, messageID, topic, qos, publishTimeout, message)
 
 	return err
 }
 
-func sendControlMessage(mqttClient MQTT.Client, logger *logrus.Entry, topic string, qos byte, clientID domain.ClientID, messageType string, content *protocol.CommandMessageContent) (*uuid.UUID, error) {
+func sendControlMessage(mqttClient MQTT.Client, logger *logrus.Entry, topic string, qos byte, publishTimeout time.Duration, clientID domain.ClientID, messageType string, content *protocol.CommandMessageContent) (*uuid.UUID, error) {
 
 	messageID, message, err := protocol.BuildControlMessage(messageType, content)
 
@@ -44,12 +45,12 @@ func sendControlMessage(mqttClient MQTT.Client, logger *logrus.Entry, topic stri
 
 	logger.Debug("Sending control message to connected client")
 
-	err = sendMessage(mqttClient, logger, clientID, messageID, topic, qos, message)
+	err = sendMessage(mqttClient, logger, clientID, messageID, topic, qos, publishTimeout, message)
 
 	return messageID, err
 }
 
-func sendMessage(mqttClient MQTT.Client, logger *logrus.Entry, clientID domain.ClientID, messageID *uuid.UUID, topic string, qos byte, message interface{}) error {
+func sendMessage(mqttClient MQTT.Client, logger *logrus.Entry, clientID domain.ClientID, messageID *uuid.UUID, topic string, qos byte, publishTimeout time.Duration, message interface{}) error {
 
 	logger = logger.WithFields(logrus.Fields{"message_id": messageID, "client_id": clientID})
 
@@ -63,7 +64,7 @@ func sendMessage(mqttClient MQTT.Client, logger *logrus.Entry, clientID domain.C
 	logger.Debug("Sending message to connected client on topic: ", topic, " qos: ", qos)
 
 	token := mqttClient.Publish(topic, qos, false, messageBuffer.Bytes())
-	if token.Wait() && token.Error() != nil {
+	if token.WaitTimeout(publishTimeout) && token.Error() != nil {
 		logger := logger.WithFields(logrus.Fields{"error": token.Error()})
 		logger.Error("Error sending a message to MQTT broker")
 		metrics.messagePublishedFailureCounter.Inc()
