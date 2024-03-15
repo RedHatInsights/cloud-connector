998e37fc1ca671c468554839f36740e3bbcc1434
Derek Horton <dehort@redhat.com>
2024-03-15 15:58:51 -0500

    Testing

--- test_utils/run_client_0.sh
+++ test_utils/run_client_0.sh
@@ -8,6 +8,7 @@ BROKER="ssl://localhost:8883"
 WORKING_DIR=/home/dehort/dev/go/src/github.com/RedHatInsights/cloud-connector
 WORKING_DIR=./dev/test_client
 
-./test_client -broker $BROKER -connection_count 1 -cert ${WORKING_DIR}/client-0-cert.pem -key ${WORKING_DIR}/client-0-key.pem
+#./test_client -broker $BROKER -connection_count 1 -cert ${WORKING_DIR}/client-0-cert.pem -key ${WORKING_DIR}/client-0-key.pem
+./auto_test_client -broker $BROKER -connection_count 1 -cert ${WORKING_DIR}/client-0-cert.pem -key ${WORKING_DIR}/client-0-key.pem -cloud-connector "http://localhost:8081"
 
 
