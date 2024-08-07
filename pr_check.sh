9aad959f110358dde873b9567c5d0f724442280d
Derek Horton <dehort@redhat.com>
2024-08-07 10:24:19 -0500

    Fix component name in pr_check.sh

--- pr_check.sh
+++ pr_check.sh
@@ -4,7 +4,7 @@
 # Options that must be configured by app owner
 # --------------------------------------------
 APP_NAME="cloud-connector"  # name of app-sre "application" folder this component lives in
-COMPONENT_NAME="cloud-connector-api"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+COMPONENT_NAME="cloud-connector"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
 IMAGE="quay.io/cloudservices/cloud-connector"
 IQE_CJI_TIMEOUT="30m"
 EXTRA_DEPLOY_ARGS="-p playbook-dispatcher/CLOUD_CONNECTOR_IMPL=impl"
