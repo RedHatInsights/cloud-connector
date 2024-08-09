c7717aeaedac2dfae87627dd67a9666a21206252
Derek Horton <dehort@redhat.com>
2024-08-09 13:13:19 -0500

    Add a test to make sure that we update an existing tenant-less entry if we find a org-id later

--- internal/connection_repository/sql_connection_registrar_test.go
+++ internal/connection_repository/sql_connection_registrar_test.go
@@ -5,8 +5,10 @@ package connection_repository
 
 import (
 	"context"
+	"database/sql"
 	"encoding/json"
 	"testing"
+	"time"
 
 	"github.com/RedHatInsights/cloud-connector/internal/config"
 	"github.com/RedHatInsights/cloud-connector/internal/domain"
@@ -89,7 +91,97 @@ func TestSqlConnectionRegistrar(t *testing.T) {
 }
 
 func TestSqlConnectionRegistrarUpdateTenantlessConnection(t *testing.T) {
-	t.Fatal("Implement this test!!", nil)
+	cfg := config.GetConfig()
+
+	database, err := db.InitializeDatabaseConnection(cfg)
+	if err != nil {
+		t.Fatal("Unable to connect to database: ", err)
+	}
+
+	orgID := domain.OrgID("")
+	account := domain.AccountID("")
+	clientID := domain.ClientID("update-tenantless-test-client-1")
+	dispatchersString := "{\"satellite\": {\"version\": \"0.2\"}}"
+
+	var dispatchers map[string]interface{}
+	err = json.Unmarshal([]byte(dispatchersString), &dispatchers)
+	if err != nil {
+		t.Fatal("unexpected error while creating dispatchers json doc", err)
+	}
+
+	var connectionRegistrar ConnectionRegistrar
+	connectionRegistrar, err = NewSqlConnectionRegistrar(cfg, database)
+	if err != nil {
+		t.Fatal("unexpected error while creating the SqlConnectionRegistrar", err)
+	}
+
+	verifyConnectionCountByClientID(t, database, clientID, 0)
+
+	connectorClientState := domain.ConnectorClientState{
+		OrgID:       orgID,
+		Account:     account,
+		ClientID:    clientID,
+		Dispatchers: dispatchers,
+	}
+
+	err = connectionRegistrar.Register(context.TODO(), connectorClientState)
+	if err != nil {
+		t.Fatal("unexpected error while registering a connection", err)
+	}
+
+	verifyConnectionCountByClientID(t, database, connectorClientState.ClientID, 1)
+
+	connectorClientState.OrgID = "1234"
+	connectorClientState.Account = "4321"
+
+	err = connectionRegistrar.Register(context.TODO(), connectorClientState)
+	if err != nil {
+		t.Fatal("unexpected error while registering a connection", err)
+	}
+
+	verifyConnectionCountByClientID(t, database, connectorClientState.ClientID, 1)
+
+	err = connectionRegistrar.Unregister(context.TODO(), connectorClientState.ClientID)
+	if err != nil {
+		t.Fatal("unexpected error while registering a connection", err)
+	}
+
+}
+
+func verifyConnectionCountByClientID(t *testing.T, database *sql.DB, clientID domain.ClientID, expectedConnectionCount int) {
+
+	connectionCount, err := getConnectionCountFromDatabase(database, clientID)
+	if err != nil {
+		t.Fatal("unexpected error while looking for connections by client-id", err)
+	}
+
+	if connectionCount != expectedConnectionCount {
+		t.Fatalf("expected connection count (%d) did not match connection count found in database (%d)", expectedConnectionCount, connectionCount)
+	}
+}
+
+func getConnectionCountFromDatabase(database *sql.DB, clientId domain.ClientID) (int, error) {
+
+	sqlQuery := "SELECT COUNT(*) FROM CONNECTIONS WHERE client_id = $1"
+
+	ctx, cancel := context.WithTimeout(context.TODO(), 1*time.Second)
+	defer cancel()
+
+	statement, err := database.Prepare(sqlQuery)
+	if err != nil {
+		return 0, err
+	}
+	defer statement.Close()
+
+	var count int
+
+	err = statement.QueryRowContext(ctx, clientId).Scan(&count)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return count, nil
 }
 
 func verifyConnectorClientState(t *testing.T, expectedClientState, actualClientState domain.ConnectorClientState) {
